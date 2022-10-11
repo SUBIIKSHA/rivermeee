feba633213e173a5d6f013de2bd059d0241e41eb
SUBIIKSHA <115527998+SUBIIKSHA@users.noreply.github.com>
2022-10-11 14:03:03 +0530

    Add files via upload

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,18 @@
+import UIKit
+import Flutter
+import GoogleMaps  // Add this import
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+
+    // TODO: Add your Google Maps API key
+    GMSServices.provideAPIKey("AIzaSyAba84cvq-045t7sMabVeltI4GVH8VYiWA")
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
